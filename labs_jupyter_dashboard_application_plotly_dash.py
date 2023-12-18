a6a77b4095b2dad4bd14f495d2fa9778feb202aa
Johnny Zeng <95407892+johnny-zeng-analysis@users.noreply.github.com>
2023-12-18 14:57:02 -0500

    Update labs_jupyter_dashboard_application_plotly_dash.py

--- labs_jupyter_dashboard_application_plotly_dash.py
+++ labs_jupyter_dashboard_application_plotly_dash.py
@@ -1,4 +1,4 @@
-# Import required libraries
+# Import libraries
 import pandas as pd
 import dash
 import dash_html_components as html
@@ -6,15 +6,15 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-# Read the airline data into pandas dataframe
+# Read airline data into pandas dataframe
 spacex_df = pd.read_csv("spacex_launch_dash.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
-# Create a dash application
+# Create dash application
 app = dash.Dash(__name__)
 
-# Create an app layout
+# Create app layout
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
@@ -54,7 +54,7 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 ])
 
 # TASK 2:
-# Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+# Add a callback function for `site-dropdown` as input and `success-pie-chart` as output
 @app.callback(Output(component_id='success-pie-chart', component_property='figure'),
               Input(component_id='site-dropdown', component_property='value'))
 def get_pie_chart(entered_site):
